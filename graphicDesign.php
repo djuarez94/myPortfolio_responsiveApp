35f072a17f7f211bd6b66111478842473ac52249
David Juarez <DavidJuarez@Davids-MacBook-Pro.local>
2017-05-15 14:00:31 -0700

    test

--- graphicDesign.php
+++ graphicDesign.php
@@ -46,14 +46,12 @@ if ($name == '' || $email == '' || $subject == '' || $message == '')
 <!doctype html>
 <html lang="en">
 <head>
-  <title>Graphic Design</title>
+  <title>Web Development</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="Assets/css/styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 	<body>
@@ -220,6 +218,11 @@ if ($name == '' || $email == '' || $subject == '' || $message == '')
 
 	<script src="Assets/js/subPages.js"></script>
 	<script src="Assets/js/jquery-3.1.1.min.js"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </html>
 
